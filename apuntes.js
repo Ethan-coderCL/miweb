0a6663f1c7193e074dc9ca8d4bc093a64a51d812
Ethan Ahumada <ethan.ahumada131219@gmail.com>
2024-10-11 00:50:33 -0300

    agregue una nueva linea de codigo

--- apuntes.js
+++ apuntes.js
@@ -13,7 +13,7 @@ git add (1.- nombre del archivo/2.-  .txt /3.-  . )
 	2.- selecciona TODOS los TIPOS de archivos.
 	3.- Selecciona TODOS LOS ARCHIVOS (mala práctica).
 	4.- agregar multiples archivos separados por un espacio.
-    
+
 rm 'nombrearchivo' / Borra el archivo
 rm 'nombre origen' 'nombre a cambiar'/ cambiar el nombre del archivo
 git commit -m "aqui va comentario" / sube el o los archivos a commit
@@ -25,6 +25,7 @@ git restore --staged 'nombre archivo' /restaura un archivo del staged
 git restore / restaura archivos de una rama de trabajo
 git branch / nos muestra en que rama estamos trabajando
 git checkout -b 'nombre que le asignamos a la rama' / crea y/o te cambia a la rama
+git status / nos muestra el estado de los archivos
 cat 'nombre del archivo' / te muestra el contenido del archivo
 git merge 'nombre de la rama' / nos trae los cambios realizados a la rama en la que estamos
 git push / sube todos los cambios
